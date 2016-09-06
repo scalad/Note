464fcbb222d499482e9a54c8545f7cda9d5ed4a6
silence940109 <695412269@qq.com>
2016-09-06 15:48:54 +0800

    随机生成32位的uuid

---
+++ uuid/UuidUtil.java
@@ -0,0 +1,9 @@
+import java.util.UUID;
+
+public class UuidUtil {
+
+	public static String get32UUID() {
+		String uuid = UUID.randomUUID().toString().trim().replaceAll("-", "");
+		return uuid;
+	}
+}
